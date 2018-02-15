e7d2e6fdd04279519d208706382722fa3a9712a6
mhamed <hamoukira@gmail.com>
2018-02-15 05:05:37 +0000

    final

--- wiki_crawl.py
+++ wiki_crawl.py
@@ -2,9 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from urllib import parse
 import argparse
-from validator import url as validate_url
+from validators import url as validate_url
 from webpage_gen import write_pages
-
+"""
+this scripte will crawl wikipidia from a start url by accesing the 1st url to the next article mentioned each page
+till it reached a target url or reaching the limit of pages to access if metiond 
+example:  python wiki_crawl.py --start https://en.wikipedia.org/wiki/Literature --target https://en.wikipedia.org/wiki/Communication
+"""
 articls = list()
 
 
@@ -20,9 +24,9 @@ def main():
     print('Done!')
 
 def urls(start,target):
-    if validate_url(start) and validate_url(target) and 'wikipedia' in start and 'wikipedia' in target
-        start_url = urlparse(start).path[1:]
-        target_url = urlparse(target).path[1:]
+    if validate_url(start) and validate_url(target) and 'wikipedia' in start and 'wikipedia' in target:
+        start_url = parse.urlparse(start).path[1:]
+        target_url = parse.urlparse(target).path[1:]
         return start_url,target_url
     else:
         print('invalide urls')
@@ -35,48 +39,48 @@ def crawl_wikipidia(url, target_url,maxp):
     url = parse.urljoin(wikipidia_base, url)
     counter=1
     
-    while continue_to_crawle(url) and len(articls)!=maxp:
+    while continue_to_crawle(url, target_url) and len(articls) != maxp:
         print(counter,url)
         html = requests.get(url)
         if html.status_code == 200:
             soup = BeautifulSoup(html.text, 'html.parser')
             title=soup.title.text
             articls.append([title,url])
-            url = get_first_url(soup)
+            url = get_first_url(soup, wikipidia_base)
         else:
             print('Error {} while trying to access page {}'.format(html.status_code,url))
             exit(-1)
         counter+=1
     
-    print("Done")
 
 
-def continue_to_crawle(url):
+def continue_to_crawle(url, target_url):
     """
     this function decides if we should continue to crawle or not
     """
     global articls
     ctc=True
-    if url == target_url:
+    if parse.urlparse(url).path[1:] == target_url:
         print('Success :\n\ttarget page \'{}\' reached after crawling ({}) pages\n\tall visited urls are in pages.html'.format(target_url, len(articls)))
         articls.append(['target',target_url])
         ctc= False
-    for articl in articls:
-        if url == articl[1]:
-            print('Error: Page Visited twice :\n\turl {} was already visited'.format(url))
-            ctc= False
-            break
+    elif ctc:
+        for articl in articls:
+            if url == articl[1]:
+                print('Error: Page Visited twice :\n\turl {} was already visited'.format(url))
+                ctc= False
+                break
     else:
-        ctc= True
+        pass
     
     return ctc
 
 
-def get_first_url(soup):
+def get_first_url(soup, wikipidia_base):
     """
     this function fetchs the 1st url in a wikipidia page 
     """
-    global wikipidia_base
+    global articls
     try:
         content=soup.find(id='mw-content-text').find(class_='mw-parser-output').find('p')
         for sp in soup.find(id='mw-content-text').find(class_='mw-parser-output').find('p').find_all('span'):
@@ -88,7 +92,7 @@ def get_first_url(soup):
     except AttributeError:
         print('Dead-end page reached before getting to target page')
         print('\tall visited urls are in pages.html')
-        write_pages()
+        write_pages(articls)
         exit(-1)
 
 
